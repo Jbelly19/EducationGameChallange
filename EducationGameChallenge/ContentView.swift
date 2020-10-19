b011fe25578c51218880f8fab0b8bff07cdc5e11
Josh Belmont <jbelmont19@live.com>
2020-10-19 09:50:04 -0700

    save

--- EducationGameChallenge/ContentView.swift
+++ EducationGameChallenge/ContentView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectedTable = 1
+    @State private var isPlaying = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Group {
+            if isPlaying {
+                GameView(selectedTable: $selectedTable)
+                Button("Quit"){
+                    isPlaying = false
+                }
+            } else {
+                SettingsView(selectedTable: $selectedTable)
+                Button("Play!"){
+                    isPlaying = true
+                }
+            }
+        }
+    }
+}
+
+struct GameView: View {
+    @Binding var selectedTable: Int
+    
+    var body: some View {
+        NavigationView {
+            Text("You want to practice \(selectedTable)'s multiplication table")
+        }.navigationBarTitle("Game View")
+    }
+}
+
+struct SettingsView: View {
+    @Binding var selectedTable: Int
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Select Multiplication Table")){
+                    Stepper(value: $selectedTable, in: 1...12, step: 1) {
+                        Text("Table: \(selectedTable)")
+                    }
+                }
+
+            }
+        }.navigationBarTitle("Settings Page!")
     }
 }
 
