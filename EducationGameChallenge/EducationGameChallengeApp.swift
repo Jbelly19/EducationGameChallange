08d7cd7694288c3930f2d5f935f97805dfff83ab
Josh Belmont <jbelmont19@live.com>
2020-10-19 08:49:21 -0700

    Initial Commit

---
+++ EducationGameChallenge/EducationGameChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EducationGameChallengeApp.swift
+//  EducationGameChallenge
+//
+//  Created by Josh Belmont on 10/19/20.
+//
+
+import SwiftUI
+
+@main
+struct EducationGameChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
